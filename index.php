79d1553582f471d534c21c762f06c0ccfda0c135
Philipp Schreitmueller <philipp.schreitmueller@in.tum.de>
2014-10-27 13:21:14 +0000

    Initial commit

---
+++ index.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Philipp
+ * Date: 22.10.2014
+ * Time: 23:54
+ */
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="icon" href="favicon.ico">
+    <title>Sudoku Solver</title>
+</head>
+
+<body>
+<div class="container">
+    <div class="jumbotron">
+        <h1>Sudoku Solver</h1>
+        <h4>Simple <code>Javascript</code> Sudoku Solver programmed by <a href="http://ph91.de" target="_blank"> <strong>Philipp Schreitmueller</strong></a></h4>
+        <h4>Check out the source at <a href="#"><span
+                    class="glyphicon glyphicon-cloud-download"></span><code>BitBucket</code></a></h4>
+
+        <p></p>
+    </div>
+</div>
+
+<div class="container sudo">
+    <div class="row">
+        <div class="col-sm-4">
+            <h3><span class="glyphicon glyphicon-paperclip"></span> Rules</h3>
+            <p>Fill in numbers <mark>1</mark> to <mark>9</mark> in the way that there is</p>
+            <ul>
+                <li>not the same number in a line.</li>
+                <li>not the same number in a row.</li>
+                <li>not the same number in a <cod>3x3</cod> sub-square.</li>
+            </ul>
+
+        </div>
+        <div class="col-sm-4">
+            <form>
+                <table class="text-center">
+                    <colgroup>
+                        <col>
+                        <col>
+                        <col>
+                    <colgroup>
+                        <col>
+                        <col>
+                        <col>
+                    <colgroup>
+                        <col>
+                        <col>
+                        <col>
+                        <?php
+                        for ($i = 0; $i < 9; $i++) {
+                            if ($i == 0 | $i == 3 | $i == 6)
+                                echo "<tbody>";
+                            echo(($i == 0 | $i == 3 | $i == 6) ? "<tbdody>" : "");
+                            echo "<tr>";
+                            for ($j = 0; $j < 9; $j++) {
+                                echo '<td><input min="1" max="9" type="number"  id="' . $i . '|' . $j . '">';
+                                // echo "<td>".$j;
+                            }
+                        }
+                        ?>
+                </table>
+
+
+            </form>
+        </div>
+        <div class="col-sm-4">
+            <p class="text-success">
+                <h3>The Sudoku is currently <mark id="isValid">valid</mark>!</h3>
+                <h3>Execution time <mark id="execTime"></mark> ms.</h3>
+            </p>
+            <p>
+                <a href="#" class="btn btn-info btn-lg" id="startSolve"><span class="glyphicon glyphicon-play"></span> Solve it!</a>
+                <a href="#" class="btn btn-info btn-lg" id="clearField"><span class="glyphicon glyphicon-trash"></span> Clear</a>
+            </p>
+        </div>
+    </div>
+</div>
+
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+<script src="js/magic.js"></script>
+</body>
+</html>
---
+++ js/magic.js
@@ -0,0 +1,231 @@
+/**
+ * Created by Philipp on 26.10.2014.
+ */
+
+var SUDOKU = SUDOKU || {};
+
+SUDOKU.checkValidity = function (x, y, number, field) {
+    // check row
+    for (var i = 0; i < 9; i++) {
+        // row
+        if (field[y][i] == number)
+            return false;
+        // column
+        if (field[i][x] == number)
+            return false;
+        // square
+
+
+        var quadx = 0;
+        var quady = 0;
+        var nextPos = y*9+x;// +1;
+        if (nextPos % 9 <= 2)
+            quadx = 0;
+        else if (nextPos % 9 <= 5)
+            quadx = 3;
+        else
+            quadx = 6;
+        if (nextPos <= 26)
+            quady = 0;
+        else if (nextPos <= 53)
+            quady = 3;
+        else
+            quady = 6;
+
+
+        //
+
+        var tX = quady + Math.floor(((i + 1) / 3)) % 3;
+        var tY = (i + 1) % 3 + quadx;
+
+        if (field[tX][tY] == number)
+            return false;
+    }
+    return true;
+
+};
+
+SUDOKU.getValidExample = function () {
+    var field = [9][9];
+    field[0][0] = 1;
+    field[0][1] = 2;
+    field[1][0] = 4;
+    return field;
+};
+
+SUDOKU.getWrongExample = function () {
+    var field = [9][9];
+    field[0][0] = 1;
+    field[0][1] = 1;
+    return field;
+};
+SUDOKU.isEqual = function (s1, s2) {
+    for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++)
+            if (s1[i][j] != s2[i][j])
+                return false;
+
+
+    return true;
+};
+
+SUDOKU.isCurrentlyValid = function (field) {
+    for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++) {
+            var x = field[i][j];
+            if (x != 0) {
+                field[i][j] = 0;
+                if (!this.checkValidity(j, i, x, field))
+                    return false;
+                else
+                    field[i][j] = x;
+            }
+        }
+    return true;
+};
+
+SUDOKU.getInput = function () {
+    var field = SUDOKU.getEmptyField();
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            var t = document.getElementById(i + "|" + j).value;
+            field[i][j] = (t != "") ? parseInt(t) : 0;
+
+        }
+    }
+    return field;
+};
+
+SUDOKU.setInput = function (field) {
+    for (var i = 0; i < 9; i++) {
+        for (var j = 0; j < 9; j++) {
+            document.getElementById(i + "|" + j).value = field[i][j];
+        }
+    }
+};
+SUDOKU.getNextState = function (field) {
+    var stack = [];
+    var nextX = 0;
+    var nextY = 0;
+    var isSet = 0;
+    for (var y = 0; y < 9; y++) {
+        for (var x = 0; x < 9; x++) {
+            if (field[y][x] == 0) {
+                nextX = x;
+                nextY = y;
+                isSet = 1;
+                break;
+            }
+
+
+        }
+        if (isSet == 1)
+            break;
+    }
+    for (var z = 1; z <= 9; z++) {
+        if (SUDOKU.checkValidity(nextX, nextY, z, field)) {
+            var newEntry = this.copyState(field);
+            newEntry[nextY][nextX] = z;
+            stack.push(newEntry);
+        }
+    }
+    return stack;
+};
+SUDOKU.copyState = function (field) {
+    var ret = this.getEmptyField();
+    for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++)
+            ret[i][j] = field[i][j];
+
+    return ret;
+}
+SUDOKU.getCount = function (field) {
+    var count = 0;
+    for (var i = 0; i < 9; i++)
+        for (var j = 0; j < 9; j++)
+            if (field[i][j] != 0)
+                count++;
+    return count;
+};
+SUDOKU.getEmptyField = function () {
+    return [[0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0, 0]]
+};
+SUDOKU.contains = function (x, list) {
+    for (var i = 0; i < list.length; i++) {
+        if (this.isEqual(x, list[i]))
+            return i;
+    }
+    return -1;
+};
+
+SUDOKU.solve = function (field) {
+    var best = 0;
+    var open = [];
+    var closed = [];
+    if (!SUDOKU.isCurrentlyValid(field))
+        return null;
+    open.push(field);
+    while (open.length != 0) {
+        var x = open.pop();
+        var curCount = SUDOKU.getCount(x);
+        if (curCount > best) {
+            best = curCount;
+            SUDOKU.setInput(x);
+        }
+        console.log("Current best:" + best + "\n");
+        if (curCount == 81) {
+            return x;
+        } else {
+            closed.push(x);
+            var next = SUDOKU.getNextState(x);
+            for (var i = 0; i < next.length; i++) {
+                var y = next[i];
+                if ((SUDOKU.contains(y, open) == -1) && (SUDOKU.contains(y, closed)) == -1) {
+                    open.push(y);
+                }
+            }
+
+        }
+    }
+    return null;
+};
+
+
+$(document).ready(function () {
+    $("input").change(function (event) {
+        event.preventDefault();
+        var field = SUDOKU.getInput();
+        if (SUDOKU.isCurrentlyValid(field)) {
+            $("#isValid").text("valid");
+            $("#isValid").parent().parent().addClass("text-success");
+            $("#isValid").parent().parent().removeClass("text-danger")
+
+        } else {
+            $("#isValid").html("not valid");
+            $("#isValid").parent().parent().addClass("text-danger");
+            $("#isValid").parent().parent().removeClass("text-danger");
+
+        }
+
+    });
+    $("#startSolve").click(function (event) {
+        event.preventDefault();
+        var field = SUDOKU.getInput();
+        var start= Date.now();
+        var solution = SUDOKU.solve(field);
+        var end= Date.now();
+        $('input[type="number"]').attr("disabled");
+        $("#execTime").text(end-start);
+
+        if (solution != null) {
+            SUDOKU.setInput(solution);
+            $("#startSolve").children().addClass("glyphicon glyphicon-ok");
+        }
+    });
+    $("#clearField").click(function (event) {
+        event.preventDefault();
+        $("#startSolve").children().removeClass("glyphicon glyphicon-ok");
+        $('input[type="number"]').val("");
+        $('input[type="number"]').removeAttr("disabled");
+    });
+});
